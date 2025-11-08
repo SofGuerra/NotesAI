3b27e8a593653c11b84bc39437910f07e13a4ab2
Sofia Guerra <sofiaguerraquintero@gmail.com>
2025-11-08 11:48:04 -0500

    Initial Commit

---
+++ NotesAI/NotesAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  NotesAIApp.swift
+//  NotesAI
+//
+//  Created by Sofia Guerra on 2025-11-08.
+//
+
+import SwiftUI
+
+@main
+struct NotesAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
