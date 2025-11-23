5a94f4f0633f37666eac916010414b587f96410c
Jimena <jimemarinp@gmail.com>
2025-11-22 23:45:47 -0500

    Jimena Marin, final commit

---
+++ NotesAI/View/HomePageView.swift
@@ -0,0 +1,18 @@
+//
+//  HomePageView.swift
+//  NotesAI
+//
+//  Created by Jimena Marin on 2025-11-18.
+//
+
+import SwiftUI
+
+struct HomePageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HomePageView()
+}
---
+++ NotesAI/View/NoteArchiveView.swift
@@ -0,0 +1,18 @@
+//
+//  NoteArchiveView.swift
+//  NotesAI
+//
+//  Created by Jimena Marin on 2025-11-18.
+//
+
+import SwiftUI
+
+struct NoteArchiveView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NoteArchiveView()
+}
