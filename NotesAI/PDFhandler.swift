d4dcbe53ae30f78cdf87925e2d1f5949845c12eb
Sofia Guerra <sofiaguerraquintero@gmail.com>
2025-11-24 21:33:41 -0500

    Colors Added, tags filter mplemented, PDF export

---
+++ NotesAI/PDFhandler.swift
@@ -0,0 +1,8 @@
+//
+//  PDFhandler.swift
+//  NotesAI
+//
+//  Created by Sofia Guerra on 2025-11-24.
+//
+
+import Foundation
---
+++ NotesAI/View/TagFilterMenu.swift
@@ -0,0 +1,8 @@
+//
+//  TagFilterMenu.swift
+//  NotesAI
+//
+//  Created by Sofia Guerra on 2025-11-24.
+//
+
+import Foundation
